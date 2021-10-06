9413b2a372f74ac5591d39a0264dfb57d582cb0f
Moxi43 <vostretsov0505@gmail.com>
2021-10-06 17:08:06 +0400

    1672.go

---
+++ easy/1672.go
@@ -0,0 +1,22 @@
+// Link to the problem: https://leetcode.com/problems/richest-customer-wealth/
+
+func maximumWealth(accounts [][]int) int {
+    max_val := 0
+    for _, acc := range accounts {
+        max_val = maxInt(max_val, sumArr(acc))
+    }
+    return max_val
+}
+
+func maxInt(x int, y int) int {
+    if y > x { return y }
+    return x
+}
+
+func sumArr(arr []int) int {
+    sum := 0
+    for _, val := range arr {
+        sum += val
+    }
+    return sum
+}
